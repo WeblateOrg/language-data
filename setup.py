a9a1d4c7c5b00f41d9a11175b2b31d0fd83d601a
Michal Čihař <michal@cihar.com>
2020-09-29 11:30:09 +0200

    Add mo compilation to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 # Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
 #
-# This file is part of Weblate Client <https://github.com/WeblateOrg/wlc>
+# This file is part of Weblate Client <https://github.com/WeblateOrg/language-data>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,11 +17,54 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-"""Setup file for easy installation."""
+
+import os
+from distutils.command.build import build
+from distutils.core import Command
+from distutils.dep_util import newer
+from glob import glob
+from itertools import chain
 
 from setuptools import setup
+from setuptools.command.build_py import build_py
+from translate.tools.pocompile import convertmo
+
+LOCALE_MASKS = [
+    "weblate_language_data/locale/*/LC_MESSAGES/*.po",
+]
 
 with open("requirements.txt") as handle:
     REQUIRES = handle.read().split()
 
-setup(install_requires=REQUIRES)
+
+class BuildMo(Command):
+    description = "update MO files to match PO"
+    user_options = []
+
+    def initialize_options(self):
+        self.build_base = None
+
+    def finalize_options(self):
+        self.set_undefined_options("build", ("build_base", "build_base"))
+
+    def run(self):
+        for name in chain.from_iterable(glob(mask) for mask in LOCALE_MASKS):
+            output = os.path.splitext(name)[0] + ".mo"
+            if not newer(name, output):
+                continue
+            print(f"compiling {name} -> {output}")
+            with open(name) as pofile, open(output, "wb") as mofile:
+                convertmo(pofile, mofile, None)
+
+
+class WeblateBuild(build):
+    """Override the default build with new subcommands."""
+
+    # The build_mo has to be before build_data
+    sub_commands = [("build_mo", lambda self: True)] + build.sub_commands
+
+
+setup(
+    install_requires=REQUIRES,
+    cmdclass={"build_mo": BuildMo, "build": WeblateBuild},
+)
