72e946a13dffc1dc306d6a0a48b13020251d87f5
Michal Čihař <michal@cihar.com>
2020-09-29 10:49:38 +0200

    Add stub for Python module with language data

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#
+# Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate Client <https://github.com/WeblateOrg/wlc>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Setup file for easy installation."""
+
+from setuptools import setup
+
+with open("requirements.txt") as handle:
+    REQUIRES = handle.read().split()
+
+setup(install_requires=REQUIRES)
