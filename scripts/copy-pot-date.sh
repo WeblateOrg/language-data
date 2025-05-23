f4d77362fa5eb140e7a5854a49c5664685c74df5
Michal Čihař <michal@cihar.com>
2025-05-23 11:21:54 +0200

    fix: correctly copy date when udating POT files
    This avoids updates containing just timestamp change.

--- scripts/copy-pot-date.sh
+++ scripts/copy-pot-date.sh
@@ -4,10 +4,10 @@
 #
 # SPDX-License-Identifier: MIT
 
-REGX='^"POT-Creation-Date:.*'
+REGX='^\("POT-Creation-Date: [0-9 +:-]*\)'
 
 # Grab date from other branch
-REPL=$(grep "$REGX" "$1" | sed -e 's/\\\\/\\\\\\\\/')
+REPL=$(sed -n "s/$REGX.*/\\1/p" "$1")
 
 # Push it into other files
 sed -i -e "s/$REGX/$REPL/" "$2"
