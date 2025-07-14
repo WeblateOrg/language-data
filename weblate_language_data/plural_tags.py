b290295eb929d280396635a9bba48270733c087f
Michal Čihař <michal@cihar.com>
2025-07-14 15:27:59 +0200

    feat: include list of additional decimal tags
    This will make it possible to implement workaround for
    https://github.com/WeblateOrg/weblate/issues/7520 in the
    translate-toolkit.

--- weblate_language_data/plural_tags.py
+++ weblate_language_data/plural_tags.py
@@ -725,6 +725,16 @@ DECIMAL_PLURAL_TAGS = {
     "zh": ["other"],
     "zu": ["one", "other"],
 }
+DECIMAL_EXTRA_TAGS = {
+    "be": ["other"],
+    "cs": ["many"],
+    "gv": ["many"],
+    "lt": ["many"],
+    "pl": ["other"],
+    "ru": ["other"],
+    "sk": ["many"],
+    "uk": ["other"],
+}
 QT_PLURAL_TAGS = {
     "aa": ["one", "other"],
     "ab": ["one", "other"],
