75c8707de3f5e7d30569070a6af2c10e28f5489e
Michal Čihař <michal@cihar.com>
2026-02-13 16:50:12 +0100

    feat: count 150/419 regions population

--- scripts/export-cldr-population.py
+++ scripts/export-cldr-population.py
@@ -6,11 +6,36 @@
 
 import json
 from collections import defaultdict
+from collections.abc import Generator
 
 MAPPING = {
     "zh": "zh_Hans",
     "pa_Arab": "pa",
 }
+REGIONS: list[str] = [
+    "150",
+    "419",
+]
+REGION_LANGUAGES: set[str] = {"en", "es"}
+
+with open(
+    "modules/cldr-json/cldr-json/cldr-core/supplemental/territoryContainment.json",
+) as handle:
+    CONTAINMENT = json.load(handle)["supplemental"]["territoryContainment"]
+
+
+def get_region_countries(text: str) -> Generator[str]:
+    for code in CONTAINMENT[text]["_contains"]:
+        if code.isdigit():
+            yield from get_region_countries(code)
+        else:
+            yield code
+
+
+REGION_COUNTRIES: dict[str, str] = {}
+for code in REGIONS:
+    for country in get_region_countries(code):
+        REGION_COUNTRIES[country] = code
 
 with open(
     "modules/cldr-json/cldr-json/cldr-core/supplemental/territoryInfo.json",
@@ -26,6 +51,8 @@ with open(
             factor = float(data["_populationPercent"]) / 100
             languages[language] += population * factor
             languages[f"{language}_{code}"] += population * factor
+            if code in REGION_COUNTRIES and language in REGION_LANGUAGES:
+                languages[f"{language}_{REGION_COUNTRIES[code]}"] += population * factor
 
 with open("population.csv", "w") as handle:
     handle.write("code,population\n")
