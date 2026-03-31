8a9b4e6d648b1ec34687aae2634c6efa780c8a13
Michal Čihař <michal@cihar.com>
2026-03-31 13:00:18 +0200

    fix: improve RTL data
    - use CLDR likely subtags dataset to guess direction based on the script
    - revert w3c originated changes as the page lists languages which use
      RTL in some script variant and not by default
    - This reverts commit 8fb5e0770c3ac33954597a793d57e2181549a688.
    
    Fixes #2357

--- scripts/export-cldr-orientation.py
+++ scripts/export-cldr-orientation.py
@@ -8,6 +8,19 @@ import csv
 import json
 from pathlib import Path
 
+RTL_SCRIPTS = [
+    "Arab",
+    "Hebr",
+    "Syrc",
+    "Thaa",
+    "Nkoo",
+    "Adlm",
+    "Mand",
+    "Samr",
+]
+# Tfng is not listed as only some languages are RTL
+RTL_TAGS = [f"-{script}-" for script in RTL_SCRIPTS]
+
 # Read languages
 with open("languages.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
@@ -22,6 +35,7 @@ with open("rtl.csv") as csvfile:
     RTLS = list(reader)
     RTL_CODES = {lang[0] for lang in RTLS}
 
+# Process layout data
 LAYOUTDIR = Path("modules/cldr-json/cldr-json/cldr-misc-full/main/")
 
 for layout_file in LAYOUTDIR.glob("*/layout.json"):
@@ -36,6 +50,14 @@ for layout_file in LAYOUTDIR.glob("*/layout.json"):
             RTL_CODES.add(code)
         elif character_order != "left-to-right":
             print(f"Unknown order for {code}: {character_order})")
+
+# Process likely subtags for default scripts
+SUBTAGS = Path("modules/cldr-json/cldr-json/cldr-core/supplemental/likelySubtags.json")
+json_text = SUBTAGS.read_text()
+data = json.loads(json_text)
+for code, subtags in data["supplemental"]["likelySubtags"].items():
+    if code in LANGUAGE_CODES and any(tag in subtags for tag in RTL_SCRIPTS):
+        RTL_CODES.add(code)
 
 with open("rtl.csv", "w") as handle:
     handle.write("code,\n")
